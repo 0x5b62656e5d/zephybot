c5119503fccc16e366f67d2bc143348e7bbddd6d
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2023-09-17 11:24:30 +0000

    Added permissions to ping.js
    Only the specified user id can run ?ping

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,5 +1,9 @@
 module.exports.run = (client, message, args) => {
-    message.channel.send(`# pong   🏓\n${Date.now() - message.createdTimestamp}ms`);
+  if (message.author.id === '936640230272942091') {
+    return message.channel.send(`# pong   🏓\n${Date.now() - message.createdTimestamp}ms`);
+  } else {
+    return message.channel.send("You don't have the permissions to run `?ping`");
+  }
 };
   
 module.exports.name = "ping";
