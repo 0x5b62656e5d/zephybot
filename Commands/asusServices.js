93c5246050dc86a73c5a127c59616b3e09e072d3
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2023-09-18 06:37:07 +0800

    Removed unnecessary imports

--- Commands/asusServices.js
+++ Commands/asusServices.js
@@ -1,5 +1,3 @@
-const { EmbedBuilder } = require('discord.js');
-
 const string = "Q: *I ran the uninstaller but when I press M4/ROG, AC still pops up*\nA: Reboot then try again. If that doesn't work, open GHelper, Extras menu, and at the bottom, stop the Asus services\n\nNote: It's generally a good idea to reboot after an installation or an uninstallation of an app. Helps clean up system files and restart system processes to make sure everything's running right.";
 
 module.exports.run = (client, message, args) => {
--- Commands/benchmarks.js
+++ Commands/benchmarks.js
@@ -1,5 +1,3 @@
-const { EmbedBuilder } = require('discord.js');
-
 const string = "- CPU\nAIDA64 - <https://www.aida64.com/downloads>\nCinebench - <https://www.maxon.net/en/downloads/cinebench-2024-downloads>\n\n- GPU\nAIDA64 - <https://www.aida64.com/downloads>\nFurmark - <>https://geeks3d.com/furmark/\nHeaven, Valley, Superposition - <https://benchmark.unigine.com/>\n\n- Combined\n3DMark - <https://www.3dmark.com/> /  <https://store.steampowered.com/app/223850/3DMark/> (*Demo*)\nAIDA64 - <https://rentry.org/stresstest> (*How-to*)\n\nHowever, for simpleness, I suggest just sticking to 3DMark, as it provides a pretty detailed benchmark analysis.";
 
 module.exports.run = (client, message, args) => {
