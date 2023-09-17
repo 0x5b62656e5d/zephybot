af59de5e26a12bd0552c5b6e5936fc3f56a20cc2
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2023-09-17 19:25:34 +0800

    Changed indent spacing

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,9 +1,9 @@
 module.exports.run = (client, message, args) => {
-  if (message.author.id === '936640230272942091') {
-    return message.channel.send(`# pong   🏓\n${Date.now() - message.createdTimestamp}ms`);
-  } else {
-    return message.channel.send("You don't have the permissions to run `?ping`");
-  }
+  	if (message.author.id === '936640230272942091') {
+    	return message.channel.send(`# pong   🏓\n${Date.now() - message.createdTimestamp}ms`);
+  	} else {
+    	return message.channel.send("You don't have the permissions to run `?ping`");
+  	}
 };
-  
+
 module.exports.name = "ping";
