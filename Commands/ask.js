d70381e4086bc12c44662b2b0a9572d81ef3979b
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2023-09-20 15:01:53 +0800

    Modified ask text

--- Commands/ask.js
+++ Commands/ask.js
@@ -1,4 +1,4 @@
-const string = "Don't ask to ask. Just ask your question. Asking \"Does anyone have a Strix?\" or \"Does anyone have a X13?\" isn't going to help you.\n\nThis is an example of a good question: \"My 6700s G14 keeps suddenly freezing when I launch a game. Why does this happen?\"";
+const string = "Don't ask to ask. Just ask your question. Asking \"Does anyone have a Strix?\" or \"Does anyone have a X13?\" isn't going to help you.\n\nThis is an example of a good question: \"My 6700s G14 keeps suddenly freezing when I launch a game. Why does this happen?\"\nThis is an example of a bad question: \"Does anyone have a G14 2020?\"";
 
 module.exports.run = (client, message, args) => {
     message.channel.send(`${string}`);
