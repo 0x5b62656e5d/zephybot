e862e0e90ab2d7c1b58cec99d1370cded8879caa
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2023-09-23 19:20:17 +0800

    Made ping command ephemeral

--- src/Commands/ping.js
+++ src/Commands/ping.js
@@ -5,9 +5,6 @@ module.exports = {
     },
  
     run: ({ interaction, client, handler }) => {
-		if (interaction.user.id != '936640230272942091') {
-            return interaction.reply("You don't have the permissions to run this command");
-        }
-        interaction.reply(`# pong   🏓\n${Date.now() - interaction.createdTimestamp}ms`);
+        interaction.reply({ content: `# pong   🏓\n${Date.now() - interaction.createdTimestamp}ms`, ephemeral: true });
     },
 };
