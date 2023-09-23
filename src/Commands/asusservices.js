b4f3a9f3061460e8ad65e4c3840bdeb563b0bf1a
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2023-09-23 21:50:55 +0800

    Modified asusservices text

--- src/Commands/asusservices.js
+++ src/Commands/asusservices.js
@@ -1,4 +1,4 @@
-const {SlashCommandBuilder} = require('discord.js');const string = "Q: *I ran the uninstaller but when I press M4/ROG, AC still pops up*\nA: Reboot then try again. If that doesn't work, open GHelper, Extras menu, and at the bottom, stop the Asus services\n\nNote: It's generally a good idea to reboot after an installation or an uninstallation of an app. Helps clean up system files and restart system processes to make sure everything's running right.";
+const {SlashCommandBuilder} = require('discord.js');const string = "Q: *I ran the uninstaller but when I press M4/ROG, AC still pops up*\nA: Reboot then try again. If that doesn't work, open GHelper, Extras menu, and at the bottom, stop the Asus services\n\nNote: It's generally a good idea to reboot after an installation or an uninstallation of an app. Helps clean up system files and restart system processes to make sure everything's running right.\n\n***LAST RESORT***: If uninstalling AC ***and*** stopping all services from GHelper didn't work, disable armory crate control interface in BIOS.";
 
 module.exports = {
     data: {
