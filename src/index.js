2a3223a946f6b905a0c69f8eeb80e0433322c467
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2023-09-23 15:38:15 +0800

    Added commandkit for slash command support

--- src/index.js
+++ src/index.js
@@ -4,45 +4,23 @@ require("dotenv").config();
 
 const Discord = require("discord.js");
 const client = new Discord.Client({intents: [Discord.GatewayIntentBits.Guilds, Discord.GatewayIntentBits.GuildMessages, Discord.GatewayIntentBits.MessageContent]});
-const {REST, Routes} = require('discord.js');
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+const {CommandKit} = require('commandkit');
+const path = require('path');
 
-(async() => {
-	try {
-		const data = await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
-        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-    } catch (error) {
-		console.log(`Error: ${error}`);
-    }
-})();
+app.listen(3000, () => {
+	console.log("Zephybot is awake");
+});
 
-client.on("interactionCreate", interaction => {
-	if (!interaction.isChatInputCommand()) return;
-	const command = interaction.commandName;
+client.on("ready", async () => {
+	console.log("Slash commands initialized");
+})
+
+new CommandKit({
+    client,
+    commandsPath: path.join(__dirname, 'Commands'),
+    devGuildIds: ['1064736062125113485'],
+    devUserIds: ['936640230272942091'],
 });
 
 client.login(process.env.TOKEN);
-
-const fs = require("fs");
-const prefix = "?";
-client.commands = new Discord.Collection();
-const commands2 = fs.readdirSync("./Commands").filter(file => file.endsWith(".js"));
-const commandslist = [];
-for (file of commands2) {
-	const commandName = file.split(".")[0];
-	commandslist.push(commandName);
-}
-console.log(commandslist);
-
-// client.on("messageCreate", message => {
-// 	if (message.content.startsWith(prefix)) {
-// 		const args = message.content.slice(prefix.length).trim().split(/ +/g);
-// 		const commandName = args.shift();
-// 		const command = client.commands.get(commandName);
-// 		if (!command) {
-// 			return;
-// 		}
-// 		command.run(client, message, args);
-// 	}
-// });
