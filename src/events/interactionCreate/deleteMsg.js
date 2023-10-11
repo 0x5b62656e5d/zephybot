e6ddbaf1daca30e1ebb3bb57f5d5d564c662e360
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2023-10-11 10:56:33 +0800

    Added ephemeral message to delete button event

--- src/events/interactionCreate/deleteMsg.js
+++ src/events/interactionCreate/deleteMsg.js
@@ -4,5 +4,7 @@ module.exports = (interaction, client) => {
     const data = interaction.customId.split('.');
     if (data[0] === 'delMsg' && interaction.user.id === data[1]) {
         interaction.message.delete();
+    } else {
+        interaction.reply({ content: `You can't perform this action`, ephemeral: true });
     }
 };
