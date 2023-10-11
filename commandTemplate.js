dcdcc47c8e071f8de1a8c48edf321a85fe87b2af
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2023-10-11 17:25:56 +0800

    Updated command template

--- commandTemplate.js
+++ commandTemplate.js
@@ -1,10 +1,32 @@
+const { ButtonBuilder, ButtonStyle, ActionRowBuilder, SlashCommandBuilder } = require('discord.js');
+
+const string = "";
+
 module.exports = {
-    data: {
-        name: '',
-        description: '',
-    },
- 
+    data: new SlashCommandBuilder()
+        .setName('')
+        .setDescription('')
+        .addUserOption(option => option.setName('target').setDescription('User to tag')),
+
     run: ({ interaction, client, handler }) => {
-        interaction.reply();
+        const delMsg = new ButtonBuilder()
+            .setCustomId(`delMsg.${interaction.user.id}`)
+            .setLabel('Delete')
+            .setStyle(ButtonStyle.Danger);
+        
+        const row = new ActionRowBuilder()
+			.addComponents(delMsg);
+
+        if (interaction.options.getUser('target') === null) {
+            return interaction.reply({
+                content: `${string}`,
+                components: [row],
+            });
+        }
+        
+        interaction.reply({
+            content: `*Suggestion for <@${interaction.options.getUser('target').id}>*\n${string}`,
+            components: [row],
+        });
     },
 };
