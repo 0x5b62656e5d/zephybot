c99ff5157fc9208f0c96d35221f86e1309193fc4
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2023-10-13 13:42:38 +0800

    Fixed readability

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
-const express = require("express");
-const app = express();
+const Discord = require("discord.js");
 const fs = require("fs");
+const { CommandKit } = require('commandkit');
+const path = require('path');
 require("dotenv").config();
 
-const Discord = require("discord.js");
 const client = new Discord.Client({intents: [Discord.GatewayIntentBits.Guilds, Discord.GatewayIntentBits.GuildMessages, Discord.GatewayIntentBits.MessageContent]});
 
-const {CommandKit} = require('commandkit');
-const path = require('path');
-
-app.listen(3000, () => {
-	console.log("Zephybot is awake");
-});
-
 client.on("ready", async () => {
-	console.log("Slash commands initialized");
+	console.log("Zephybot is awake :>");
 })
 
 client.commands = new Discord.Collection();
