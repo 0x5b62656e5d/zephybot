005356ab3b1f1e25933e8284e626b95b9fa45fd7
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2024-03-05 06:31:58 +0800

    Added PCIe compatability command

---
+++ src/Commands/pciegen.js
@@ -0,0 +1,32 @@
+const { ButtonBuilder, ButtonStyle, ActionRowBuilder, SlashCommandBuilder } = require('discord.js');
+
+const string = "# PCIe compatability\nQ: *Can I install a Gen 4 PCIe SSD into my Gen 3 slot?*\nA: Yes, you can. However, the Gen 4 SSD will run at Gen 3 speeds.";
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('pciegen')
+        .setDescription('PCIe backwards compatability')
+        .addUserOption(option => option.setName('target').setDescription('User to tag')),
+
+    run: ({ interaction, client, handler }) => {
+        const delMsg = new ButtonBuilder()
+            .setCustomId(`delMsg.${interaction.user.id}`)
+            .setLabel('Delete')
+            .setStyle(ButtonStyle.Danger);
+        
+        const row = new ActionRowBuilder()
+			.addComponents(delMsg);
+
+        if (interaction.options.getUser('target') === null) {
+            return interaction.reply({
+                content: `${string}`,
+                components: [row],
+            });
+        }
+        
+        interaction.reply({
+            content: `*Suggestion for <@${interaction.options.getUser('target').id}>*\n${string}`,
+            components: [row],
+        });
+    },
+};
