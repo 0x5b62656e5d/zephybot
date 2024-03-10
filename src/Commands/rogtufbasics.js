39f48d4eac3526dcecb6974ca10bcc06bcee54c7
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2024-03-10 11:42:21 +0800

    Modified command name/title

--- src/Commands/rogtufbasics.js
+++ src/Commands/rogtufbasics.js
@@ -1,11 +1,11 @@
 const { ButtonBuilder, ButtonStyle, ActionRowBuilder, SlashCommandBuilder } = require('discord.js');
 
-const string = "# G14 Basics\n1. Crate/MyAsus replacement\n    a) Replace Armory Crate ***and*** MyAsus with GHelper if you haven't already. (Refer to `Replacing AC`)\n        I. Make sure to stop all Asus services in GHelper. (Refer to `Asus services`)\n        II. Leave GHelper settings at default. You do not need to change anything.\n2. Power modes\n    a) When on battery, use `Eco` GPU mode and `Silent` CPU mode\n    b) When plugged in, use `Optimized` GPU mode and `Balanced` CPU mode\n3. Power source\n    a) Try to use the barrel charger at all times. The USBC charger doesn't have bypass and can wear out the battery faster. (Refer to `Battery`)";
+const string = "# ROG/TUF Basics\n1. Crate/MyAsus replacement\n    a) Replace Armory Crate ***and*** MyAsus with GHelper if you haven't already. (Refer to `Replacing AC`)\n        I. Make sure to stop all Asus services in GHelper. (Refer to `Asus services`)\n        II. Leave GHelper settings at default. You do not need to change anything.\n2. Power modes\n    a) When on battery, use `Eco` GPU mode and `Silent` CPU mode\n    b) When plugged in, use `Optimized` GPU mode and `Balanced` CPU mode\n3. Power source\n    a) Try to use the barrel charger at all times. The USBC charger doesn't have bypass and can wear out the battery faster. (Refer to `Battery`)";
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('g14basics')
-        .setDescription('G14 Basic FAQ')
+        .setName('basics')
+        .setDescription('ROG/TUF Basic FAQ')
         .addUserOption(option => option.setName('target').setDescription('User to tag')),
 
     run: ({ interaction, client, handler }) => {
