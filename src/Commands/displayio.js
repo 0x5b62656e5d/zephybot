3b0dc819fc8eb6bf3f9f7adac8dc9f8acdf1c821
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2024-03-31 13:27:57 +0800

    Added displayio command

---
+++ src/Commands/displayio.js
@@ -0,0 +1,32 @@
+const { ButtonBuilder, ButtonStyle, ActionRowBuilder, SlashCommandBuilder } = require('discord.js');
+
+const string = "# Display IO\nQ: *Which ports use dGPU and which ports use iGPU?*\n\n***HDMI*** - dGPU\n***Right USBC*** - dGPU\n***Left USBC*** - iGPU";
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('displayio')
+        .setDescription('Display output ports')
+        .addUserOption(option => option.setName('target').setDescription('User to tag')),
+
+    run: ({ interaction, client, handler }) => {
+        const delMsg = new ButtonBuilder()
+            .setCustomId(`delMsg.${interaction.user.id}`)
+            .setLabel('Delete')
+            .setStyle(ButtonStyle.Danger);
+        
+        const row = new ActionRowBuilder()
+			.addComponents(delMsg);
+
+        if (interaction.options.getUser('target') === null) {
+            return interaction.reply({
+                content: `${string}`,
+                components: [row],
+            });
+        }
+        
+        interaction.reply({
+            content: `*Suggestion for <@${interaction.options.getUser('target').id}>*\n${string}`,
+            components: [row],
+        });
+    },
+};
