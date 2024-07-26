3a570988f4ae26b9bde572be1cf891bb0b73ddc7
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2024-07-26 16:14:53 +0800

    Revert "Update drivers text"
    This reverts commit 62b1e132ddcb00b640c16e06ec2da2b883ef1227.

--- src/Commands/drivers.js
+++ src/Commands/drivers.js
@@ -1,6 +1,6 @@
 const { ButtonBuilder, ButtonStyle, ActionRowBuilder, SlashCommandBuilder } = require('discord.js');
 
-const string = "# Drivers\n\nLet Windows updates take care of your drivers. Asus drivers can get pretty old, as they don't update them often. If you don't have internet access, use ethernet or plug your phone into your laptop for hotspot.\nFor graphics drivers, install them from their respective vendors.";
+const string = "# Drivers\nLet Windows updates take care of your drivers. Asus drivers can get pretty old, as they don't update them often. If you don't have internet access, use ethernet or plug your phone into your laptop for hotspot.\nFor graphics drivers, install them from their respective vendors.";
 
 module.exports = {
     data: new SlashCommandBuilder()
