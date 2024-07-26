6cae9fd1660a4052d4152e8c45a5e1e6b0c8ae5e
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2024-07-26 16:15:02 +0800

    Revert "Update bootsound text"
    This reverts commit 52b3a6a55aea019349ba4285ba6724905696183b.

--- src/Commands/bootsound.js
+++ src/Commands/bootsound.js
@@ -1,6 +1,6 @@
 const { ButtonBuilder, ButtonStyle, ActionRowBuilder, SlashCommandBuilder } = require('discord.js');
 
-const string = "# Disabling laptop boot sound\n\n### Easy way:\nIn GHelper, open the `Extra` menu, and at the bottom, uncheck the `Boot Sound` option.\n### Complicated way:\nBoot into BIOS (While laptop is turing on from a shutdown or reboot, keep mashing the `F2` button until you enter BIOS). Press `F7` for `Advanced Mode`. Navigate to the `Boot` menu, then select an entry that has something like \"Animation post-logo configuration\" or something like that. You should see a boot sound option. Disable it, then save and reboot.";
+const string = "# Disabling laptop boot sound\n### Easy way:\nIn GHelper, open the `Extra` menu, and at the bottom, uncheck the `Boot Sound` option.\n### Complicated way:\nBoot into BIOS (While laptop is turing on from a shutdown or reboot, keep mashing the `F2` button until you enter BIOS). Press `F7` for `Advanced Mode`. Navigate to the `Boot` menu, then select an entry that has something like \"Animation post-logo configuration\" or something like that. You should see a boot sound option. Disable it, then save and reboot.";
 
 module.exports = {
     data: new SlashCommandBuilder()
