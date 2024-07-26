ab9ad1350dbc37dfaf281dc0a1dc8b5197d71ab5
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2024-07-26 15:04:38 +0800

    Fix cleaning command message

--- src/Commands/cleaning.js
+++ src/Commands/cleaning.js
@@ -1,6 +1,6 @@
 const { ButtonBuilder, ButtonStyle, ActionRowBuilder, SlashCommandBuilder } = require('discord.js');
 
-const string = "# Cleaning fans\nQ: *Do I need to unplug the battery when cleaning the fans?*\nA: No, I strongly suggest against it. Lots of people come in here or the Zephyrus subreddit because their board got shorted/fried in the process. Just shut down your laptop, and clean the laptop.\n\nSecondly, I also strongly suggest using brushes. Being too rough can damage/break the fins. A can of compressed air (blow gently) or an electric duster should do the job. Additionally, ***do not*** let the fans spin on its own when blowing on it. This can generate current and can damage components.";
+const string = "# Cleaning fans\nQ: *Do I need to unplug the battery when cleaning the fans?*\nA: No, I strongly suggest against it. Lots of people come in here or the Zephyrus subreddit because their board got shorted/fried in the process. Just shut down your laptop, and clean the laptop.\n\nSecondly, I also strongly suggest against using brushes. Being too rough can damage/break the fins. A can of compressed air (blow gently) or an electric duster should do the job. Additionally, ***do not*** let the fans spin on its own when blowing on it. This can generate current and can damage components.";
 
 module.exports = {
     data: new SlashCommandBuilder()
