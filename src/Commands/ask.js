f8ffbc775eb61ba7dbac0980ee5cc0677b39413e
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2024-09-16 00:05:03 +0800

    Update ask text

--- src/Commands/ask.js
+++ src/Commands/ask.js
@@ -1,6 +1,7 @@
 const { ButtonBuilder, ButtonStyle, ActionRowBuilder, SlashCommandBuilder } = require('discord.js');
 
-const string = "Don't ask to ask. Just ask your question. Asking \"Does anyone have a Strix?\" or \"Does anyone have a X13?\" isn't going to help you.\n\nThis is an example of a good question: \"My 6700s G14 keeps suddenly freezing when I launch a game. Why does this happen?\"\nThis is an example of a bad question: \"Does anyone have a G14 2020?\"\n\nAlso, be sure to provide enough info. It's hard to help you out when we don't know anything about the problem.";
+// const string = "Don't ask to ask. Just ask your question. Asking \"Does anyone have a Strix?\" or \"Does anyone have a X13?\" isn't going to help you.\n\nThis is an example of a good question: \"My 6700s G14 keeps suddenly freezing when I launch a game. Why does this happen?\"\nThis is an example of a bad question: \"Does anyone have a G14 2020?\"\n\nAlso, be sure to provide enough info. It's hard to help you out when we don't know anything about the problem.";
+const string = "https://dontasktoask.com";
 
 module.exports = {
     data: new SlashCommandBuilder()
