68a50749b3901d72f27632b0e455d750ed45bde4
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2024-10-29 21:23:11 +0800

    Fix expired ROG Linux invite link

--- src/Commands/linux.js
+++ src/Commands/linux.js
@@ -1,6 +1,6 @@
 const { ButtonBuilder, ButtonStyle, ActionRowBuilder, SlashCommandBuilder } = require('discord.js');
 
-const string = "# Linux\n\nQ: *I'm having issues in linux. What do I do?*\nA: The ROG community made a linux guide on <https://asus-linux.org>. If you can't find a fix there, head over to the ROG Linux discord or the Asus ROG subreddit.\n\nDiscord link: discord.gg/4ZKGd7Un5t\nROG Reddit: <https://reddit.com/r/ASUSROG>";
+const string = "# Linux\n\nQ: *I'm having issues in linux. What do I do?*\nA: The ROG community made a linux guide on <https://asus-linux.org>. If you can't find a fix there, head over to the ROG Linux discord or the Asus ROG subreddit.\n\nDiscord link: https://discord.gg/A5CyZEw3se\nROG Reddit: <https://reddit.com/r/ASUSROG>";
 
 module.exports = {
     data: new SlashCommandBuilder()
