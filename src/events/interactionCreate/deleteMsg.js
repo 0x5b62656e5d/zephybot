2f89a105d9befb1ddc8bb2ffd7571f79995199e0
0x5b62656e5d <dev.benjamink@gmail.com>
2025-02-23 11:15:37 +0800

    Fix uncached channel error

--- src/events/interactionCreate/deleteMsg.js
+++ src/events/interactionCreate/deleteMsg.js
@@ -1,8 +1,19 @@
-module.exports = (interaction, client) => {
+module.exports = async (interaction, client) => {
     if (!interaction.isButton()) return;
 
     const data = interaction.customId.split('.');
+
     if (data[0] === 'delMsg' && interaction.user.id === data[1]) {
+        if (!interaction.message.channel) {
+            try {
+                const channel = await client.channels.fetch(interaction.message.channelId);
+                interaction.message.channel = channel;
+            } catch (error) {
+                console.error("Failed to fetch channel: ", error);
+                return interaction.reply({ content: `Failed perform this action`, ephemeral: true });
+            }
+        }
+
         interaction.message.delete();
     } else {
         interaction.reply({ content: `You can't perform this action`, ephemeral: true });
