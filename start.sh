ce5ad93bb272d1a052a8065703a85d5caa3c7fb6
0x5b62656e5d <dev.benjamink@gmail.com>
2025-05-03 23:17:10 +0800

    Update docker scripts

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-docker build --network=host -t zephybot ~/zephybot/
-docker run -d --restart unless-stopped --network=host --name zephybot \
-  -v ~/zephybot/logs:/app/logs \
-  -v ~/zephybot/data:/app/data \
-  --env-file ~/zephybot/.env \
-  zephybot
+docker build --network=host -t zephybot ./
+docker compose up -d
