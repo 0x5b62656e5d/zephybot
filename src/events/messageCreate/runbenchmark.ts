a7cf644ba765c1a0f1dcec99d07faac97cc9e9f1
0x5b62656e5d <dev.benjamink@gmail.com>
2025-05-03 17:02:01 +0800

    Add event for benchmark advice

---
+++ src/events/messageCreate/runbenchmark.ts
@@ -0,0 +1,35 @@
+import { Events, Message } from "discord.js";
+
+const benchmarkKeywords = ["3dmark", "benchmark"];
+
+const string =
+    "Run a [3DMark demo](<https://store.steampowered.com/app/223850/3DMark/>) benchmark and send screenshots of all the graphs and the results screen. This may help us determine what's going on.";
+
+module.exports = {
+    name: Events.MessageCreate,
+    async execute(message: Message) {
+        if (message.author.bot) {
+            return;
+        }
+
+        message
+            .fetchReference()
+            .then(ref => {
+                if (
+                    ref &&
+                    benchmarkKeywords.some(keyword =>
+                        message.content.toLowerCase().includes(keyword)
+                    )
+                ) {
+                    message.reply({
+                        content: `<@${ref.author.id}>\n${string}`,
+                    });
+                } else {
+                    return;
+                }
+            })
+            .catch(error => {
+                return;
+            });
+    },
+};
