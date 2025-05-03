c17ee42b2cc01b354dda400ee317798479be23c9
0x5b62656e5d <dev.benjamink@gmail.com>
2025-05-03 21:40:42 +0800

    Fix docker container not reoslving hosts

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker build --network=host -t zephybot ~/zephybot/
-docker run -d --restart unless-stopped --name zephybot \
+docker run -d --restart unless-stopped --network=host --name zephybot \
   -v ~/zephybot/logs:/app/logs \
   -v ~/zephybot/data:/app/data \
   --env-file ~/zephybot/.env \
