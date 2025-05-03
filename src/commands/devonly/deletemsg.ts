14510532bebe594b9684798ecc13a6549f3e78c1
0x5b62656e5d <dev.benjamink@gmail.com>
2025-05-03 16:47:59 +0800

    Add error handling for deleting messages

--- src/commands/devonly/deletemsg.ts
+++ src/commands/devonly/deletemsg.ts
@@ -36,13 +36,25 @@ module.exports = {
         interaction.channel?.messages
             .fetch(messageId)
             .then(async msg => {
-                msg.delete();
+                try {
+                    msg.delete();
+                } catch (error) {
+                    console.error("[ERROR] Failed to delete message: ", error);
+
+                    return interaction.reply({
+                        content: `Failed to delete message with ID \`${messageId}\``,
+                        flags: MessageFlags.Ephemeral,
+                    });
+                }
+
                 interaction.reply({
                     content: `Message with ID \`${messageId}\` deleted!`,
                     flags: MessageFlags.Ephemeral,
                 });
             })
             .catch(error => {
+                console.error("[ERROR] Failed to fetch message: ", error);
+
                 interaction.reply({
                     content: `Message with ID \`${messageId}\` not found`,
                     flags: MessageFlags.Ephemeral,
