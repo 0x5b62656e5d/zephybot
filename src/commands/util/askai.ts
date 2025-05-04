6a571f015168a6f774b1a95ebf88b577384a455e
0x5b62656e5d <dev.benjamink@gmail.com>
2025-05-04 10:24:56 +0800

    Fix Gemini AI mistake disclaimer

--- src/commands/util/askai.ts
+++ src/commands/util/askai.ts
@@ -78,7 +78,7 @@ module.exports = {
                 }
 
                 return await interaction.editReply({
-                    content: `> ${response.replace(/\n/g, "\n> ")}-# Note that Gemini is an AI model and can make mistakes.`,
+                    content: `> ${response.replace(/\n/g, "\n> ")}\n-# Note that Gemini is an AI model and can make mistakes.`,
                     components: [row],
                 });
             } catch (error: any) {
@@ -102,13 +102,13 @@ module.exports = {
                         content: `*Suggestion for <@${target.id}>*\n\n> ${shortenedResponse.replace(
                             /\n/g,
                             "\n> "
-                        )}-# Note that Gemini is an AI model and can make mistakes.`,
+                        )}\n-# Note that Gemini is an AI model and can make mistakes.`,
                         components: [row],
                     });
                 }
 
                 return await interaction.editReply({
-                    content: `> ${shortenedResponse.replace(/\n/g, "\n> ")}-# Note that Gemini is an AI model and can make mistakes.`,
+                    content: `> ${shortenedResponse.replace(/\n/g, "\n> ")}\n-# Note that Gemini is an AI model and can make mistakes.`,
                     components: [row],
                 });
             }
