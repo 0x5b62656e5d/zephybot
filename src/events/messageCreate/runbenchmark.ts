9f8b3505b42f2f3cf3846a860045f1889affd648
0x5b62656e5d <dev.benjamink@gmail.com>
2025-05-04 21:46:41 +0800

    Update runbenchmark message event

--- src/events/messageCreate/runbenchmark.ts
+++ src/events/messageCreate/runbenchmark.ts
@@ -1,7 +1,5 @@
 import { Events, Message } from "discord.js";
 
-const benchmarkKeywords = ["3dmark", "benchmark"];
-
 const string =
     "Run a [3DMark demo](<https://store.steampowered.com/app/223850/3DMark/>) benchmark and send screenshots of all the graphs and the results screen. This may help us determine what's going on.";
 
@@ -16,12 +14,7 @@ module.exports = {
         message
             .fetchReference()
             .then(ref => {
-                if (
-                    ref &&
-                    benchmarkKeywords.some(keyword =>
-                        message.content.toLowerCase().includes(keyword)
-                    )
-                ) {
+                if (ref && message.content.toLowerCase().includes("3dmark")) {
                     message.reply({
                         content: `<@${ref.author.id}>\n${string}`,
                     });
